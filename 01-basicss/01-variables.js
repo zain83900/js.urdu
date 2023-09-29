c5eac805517f2e13d46bf59142367781e4fd2fc1
zain83900 <146279326+zain83900@users.noreply.github.com>
2023-09-29 00:29:52 +0000

    variables

---
+++ 01-basicss/01-variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "zainjan221@gmail.com"
+var password = "aloneme221"
+accountCity = "karachi"
+let accountState;
+
+// accountId =2
+accountEmail = "zinc.ali0892@.com"
+accountPassword = "12121212"
+accountCity = "lahore"
+
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
